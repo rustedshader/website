b32eeeefe5eebd0fe0a17fc9271118139faa69cb
rustedshader <rustedshader@gmail.com>
2025-05-09 10:03:38 +0530

    Add @vercel/analytics and enhance Home page content with Hindi font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import Link from "next/link";
+import { Tiro_Devanagari_Hindi } from "next/font/google";
+
+const hindi = Tiro_Devanagari_Hindi({ subsets: ['latin'], weight: '400' })
 
 export default function Home() {
   return (
     <div>
-      <div className="space-x-34 text-center">
+      <div className="space-x-34 text-center underline">
         <Link href={"/Home"}>Home</Link>
         <Link href={"/blogs"}>Blogs</Link>
         <Link href={"/photos"}>Photos</Link>
@@ -13,10 +16,13 @@ export default function Home() {
 
 
       <div className="flex flex-col items-center mt-10 px-4">
-        <div className="text-left w-full max-w-xl">
+        <div className="text-left w-full max-w-2xl border-b-2">
           <div className="font-bold text-5xl text-left">Shubhang Sharma</div>
-          <div className="font-bold mt-4">Namaste,</div>
-          <div>I am a student at UPES Dehradun currently persuing Btech Computer Science.</div>
+          <div className={`${hindi.className} font-bold text-5xl mt-10`}>नमस्ते,</div>
+          <div className="mt-4 text-lg">I am a student at UPES Dehradun, currently pursuing a B.Tech in Computer Science and Engineering.</div>
+          <div className="font-bold text-1xl">I create cool software. My work can be seen on my git</div>
+          <div>I specialize in Python and Full Stack Development.</div>
+          <div className="mb-4">I am currently learning and developing in Go and Rust.</div>
         </div>
       </div>
     </div>
